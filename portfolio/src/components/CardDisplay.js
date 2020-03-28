7b19fa34f612501e3c2f098d78fe66ad76bafafd
Erik Rodriguez <eric19.21.03@gmail.com>
2020-03-27 19:20:29 -0700

    Setup data tree and create card component for projects

---
+++ portfolio/src/components/CardDisplay.js
@@ -0,0 +1,39 @@
+import React from "react";
+import Icon7 from "../img/view-icon.png";
+import Icon8 from "../img/codefile-icon.png";
+// import ExamplePic from "../img/TemplateforSite.JPG";
+// Using image to link converter postimage.org
+
+const CardDisplay = (props) => {
+  const {
+    mainImg,
+    project_id,
+    name,
+    description,
+    deployLink,
+    githubCodeLink
+  } = props.data;
+  return (
+    <div className="workCard">
+      <img src={mainImg} alt="Project Capture" className="sizingImg" />
+      <p className="workCardText">
+        Project {project_id}
+        <br />
+        Name: {name}
+        <br />
+        Description: {description}
+      </p>
+      <div>
+        <a href={deployLink} target="_blank" rel="noopener noreferrer">
+          <img src={Icon7} alt="Code File Icon" className="projectLink" />
+        </a>
+
+        <a href={githubCodeLink} target="_blank" rel="noopener noreferrer">
+          <img src={Icon8} alt="Code File Icon" className="projectLink" />
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default CardDisplay;
--- portfolio/src/components/Work.js
+++ portfolio/src/components/Work.js
@@ -1,9 +1,110 @@
 import React from "react";
+import CardDisplay from "./CardDisplay";
 import "../App.css";
-import ExamplePic from "../img/TemplateforSite.JPG";
-import Icon7 from "../img/codefile-icon.png";
 
 const Work = () => {
+  const data = [
+    {
+      id: 1,
+      mainImg: "https://i.postimg.cc/hj1rcwSV/Templatefor-Site.jpg",
+      project_id: "01",
+      name: "Messaging App",
+      description:
+        "This app is under construction at the moment. However, Front-end will be made using React and styling library Styled-Components/React Spring. Back-end is also setup using Node.js.",
+      deployLink: "http://www.google.com",
+      githubCodeLink: "http://www.google.com"
+    },
+    {
+      id: 2,
+      mainImg: "https://i.postimg.cc/hj1rcwSV/Templatefor-Site.jpg",
+      project_id: "02",
+      name: "Tic Tac Toe App",
+      description:
+        "This app is under construction at the moment. However, Front-end will be made using React and styling library Styled-Components/React Spring. Back-end is also setup using Node.js.",
+      deployLink: "http://www.google.com",
+      githubCodeLink: "http://www.google.com"
+    },
+    {
+      id: 3,
+      mainImg: "https://i.postimg.cc/hj1rcwSV/Templatefor-Site.jpg",
+      project_id: "03",
+      name: "Messaging App",
+      description:
+        "This app is under construction at the moment. However, Front-end will be made using React and styling library Styled-Components/React Spring. Back-end is also setup using Node.js.",
+      deployLink: "http://www.google.com",
+      githubCodeLink: "http://www.google.com"
+    },
+    {
+      id: 4,
+      mainImg: "https://i.postimg.cc/hj1rcwSV/Templatefor-Site.jpg",
+      project_id: "04",
+      name: "Tic Tac Toe App",
+      description:
+        "This app is under construction at the moment. However, Front-end will be made using React and styling library Styled-Components/React Spring. Back-end is also setup using Node.js.",
+      deployLink: "http://www.google.com",
+      githubCodeLink: "http://www.google.com"
+    },
+    {
+      id: 5,
+      mainImg: "https://i.postimg.cc/hj1rcwSV/Templatefor-Site.jpg",
+      project_id: "05",
+      name: "Messaging App",
+      description:
+        "This app is under construction at the moment. However, Front-end will be made using React and styling library Styled-Components/React Spring. Back-end is also setup using Node.js.",
+      deployLink: "http://www.google.com",
+      githubCodeLink: "http://www.google.com"
+    },
+    {
+      id: 6,
+      mainImg: "https://i.postimg.cc/hj1rcwSV/Templatefor-Site.jpg",
+      project_id: "06",
+      name: "Tic Tac Toe App",
+      description:
+        "This app is under construction at the moment. However, Front-end will be made using React and styling library Styled-Components/React Spring. Back-end is also setup using Node.js.",
+      deployLink: "http://www.google.com",
+      githubCodeLink: "http://www.google.com"
+    },
+    {
+      id: 7,
+      mainImg: "https://i.postimg.cc/hj1rcwSV/Templatefor-Site.jpg",
+      project_id: "07",
+      name: "Messaging App",
+      description:
+        "This app is under construction at the moment. However, Front-end will be made using React and styling library Styled-Components/React Spring. Back-end is also setup using Node.js.",
+      deployLink: "http://www.google.com",
+      githubCodeLink: "http://www.google.com"
+    },
+    {
+      id: 8,
+      mainImg: "https://i.postimg.cc/hj1rcwSV/Templatefor-Site.jpg",
+      project_id: "08",
+      name: "Tic Tac Toe App",
+      description:
+        "This app is under construction at the moment. However, Front-end will be made using React and styling library Styled-Components/React Spring. Back-end is also setup using Node.js.",
+      deployLink: "http://www.google.com",
+      githubCodeLink: "http://www.google.com"
+    },
+    {
+      id: 9,
+      mainImg: "https://i.postimg.cc/hj1rcwSV/Templatefor-Site.jpg",
+      project_id: "09",
+      name: "Messaging App",
+      description:
+        "This app is under construction at the moment. However, Front-end will be made using React and styling library Styled-Components/React Spring. Back-end is also setup using Node.js.",
+      deployLink: "http://www.google.com",
+      githubCodeLink: "http://www.google.com"
+    },
+    {
+      id: 10,
+      mainImg: "https://i.postimg.cc/hj1rcwSV/Templatefor-Site.jpg",
+      project_id: "10",
+      name: "Tic Tac Toe App",
+      description:
+        "This app is under construction at the moment. However, Front-end will be made using React and styling library Styled-Components/React Spring. Back-end is also setup using Node.js.",
+      deployLink: "http://www.google.com",
+      githubCodeLink: "http://www.google.com"
+    }
+  ];
   return (
     <>
       <h2>Works</h2>
@@ -16,87 +117,9 @@ const Work = () => {
           </p>
         </div>
         <div className="workProjects">
-          <div key={1} className="workCard">
-            <img src={ExamplePic} alt="Project Capture" className="sizingImg" />
-            <p className="workCardText">
-              Project 001
-              <br />
-              Name: Messaging App
-              <br />
-              Description: This app is under construction at the moment.
-              However, Front-end will be made using React and styling library
-              Reactstrap/Bootstrap. Back-end is setup using Node.js.
-            </p>
-            <a
-              href="http://www.google.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img src={Icon7} alt="Code File Icon" className="projectLink" />
-            </a>
-          </div>
-
-          <div key={2} className="workCard">
-            <img src={ExamplePic} alt="Project Capture" className="sizingImg" />
-            <p className="workCardText">
-              Project 002
-              <br />
-              Name: Tic Tac Toe App
-              <br />
-              Description: This app is under construction at the moment.
-              However, Front-end will be made using React and styling library
-              Styled-Components/React Spring. Back-end is also setup using
-              Node.js.
-            </p>
-            <a
-              href="http://www.google.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img src={Icon7} alt="Code File Icon" className="projectLink" />
-            </a>
-          </div>
-
-          <div key={3} className="workCard">
-            <img src={ExamplePic} alt="Project Capture" className="sizingImg" />
-            <p className="workCardText">
-              Project 001
-              <br />
-              Name: Messaging App
-              <br />
-              Description: This app is under construction at the moment.
-              However, Front-end will be made using React and styling library
-              Reactstrap/Bootstrap. Back-end is setup using Node.js.
-            </p>
-            <a
-              href="http://www.google.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img src={Icon7} alt="Code File Icon" className="projectLink" />
-            </a>
-          </div>
-
-          <div key={4} className="workCard">
-            <img src={ExamplePic} alt="Project Capture" className="sizingImg" />
-            <p className="workCardText">
-              Project 002
-              <br />
-              Name: Tic Tac Toe App
-              <br />
-              Description: This app is under construction at the moment.
-              However, Front-end will be made using React and styling library
-              Styled-Components/React Spring. Back-end is also setup using
-              Node.js.
-            </p>
-            <a
-              href="http://www.google.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img src={Icon7} alt="Code File Icon" className="projectLink" />
-            </a>
-          </div>
+          {data.map((each) => (
+            <CardDisplay key={each.id} data={each} />
+          ))}
         </div>
       </div>
     </>
