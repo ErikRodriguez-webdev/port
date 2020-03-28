0bf8def10fc52fa09b8bbb2b9f8a14478fcc3867
Erik Rodriguez <eric19.21.03@gmail.com>
2020-03-28 16:28:18 -0700

    Add icons and name to skill cards

--- portfolio/src/components/Skill.js
+++ portfolio/src/components/Skill.js
@@ -1,10 +1,54 @@
 import React from "react";
+import Skill1 from "../img/html-5-100.png";
+import Skill2 from "../img/css3-100.png";
+import Skill3 from "../img/javascript-100.png";
+import Skill4 from "../img/react-100.png";
+import Skill5 from "../img/nodejs-100.png";
+import Skill6 from "../img/python-100.png";
+import Skill7 from "../img/ruby-100.png";
 
 const Skill = () => {
   return (
-    <div>
-      <p>Skill.js</p>
-    </div>
+    <>
+      <h2>Skill</h2>
+      <div className="skillMainBox">
+        <div className="skillCard">
+          <img src={Skill1} alt="html logo" title="HTML 5" />
+          <p>HTML 5</p>
+        </div>
+
+        <div className="skillCard">
+          <img src={Skill2} alt="css logo" title="CSS 3" />
+          <p>CSS 3</p>
+        </div>
+
+        <div className="skillCard">
+          <img src={Skill3} alt="javascript logo" title="Javascript" />
+          <p>Javascript</p>
+        </div>
+
+        <div className="skillCard">
+          <img src={Skill4} alt="react logo" title="React" />
+          <p>React</p>
+        </div>
+
+        <div className="skillCard">
+          <img src={Skill5} alt="Nodejs logo" title="HTML 5" />
+          <p>Nodejs</p>
+        </div>
+
+        <div className="skillCard">
+          <img src={Skill6} alt="Python logo" title="Python" />
+          <p>Python</p>
+        </div>
+
+        <div className="skillCard">
+          <img src={Skill7} alt="Ruby logo" title="Ruby" />
+          <p>Ruby</p>
+        </div>
+      </div>
+      <p>Developer Icons By: Icon8</p>
+    </>
   );
 };
 
