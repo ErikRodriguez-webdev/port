90c336e39bdc4032e02e5f7265058c577f728639
Erik Rodriguez <eric19.21.03@gmail.com>
2020-04-02 15:25:38 -0700

    Add home continue button and test to fix horizontal scroll bar

--- portfolio/src/components/Home.js
+++ portfolio/src/components/Home.js
@@ -1,12 +1,17 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 import "../App.css";
 
 const Home = () => {
+  const history = useHistory();
+
   return (
     <>
       <div className="homeMainBox">
         <h1>Hello and Welcome to My Port.</h1>
-        <p>CLICK HERE TO CONTINUE</p>
+        <button onClick={() => history.push("/about")}>
+          CLICK HERE TO CONTINUE
+        </button>
       </div>
     </>
   );
