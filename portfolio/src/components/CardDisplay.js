32ce085d93849c918676dda788c53db40c419021
Erik Rodriguez <eric19.21.03@gmail.com>
2020-04-01 19:56:52 -0700

    Added 3D animation to project cards

--- portfolio/src/components/CardDisplay.js
+++ portfolio/src/components/CardDisplay.js
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import { useSpring, animated } from "react-spring";
 import Icon7 from "../img/view-icon.png";
 import Icon8 from "../img/codefile-icon.png";
 // import ExamplePic from "../img/TemplateforSite.JPG";
 // Using image to link converter postimage.org
 
+//3D Card Animation
+const calculate = (x, y) => {
+  return [
+    -(y - window.innerHeight / 2) / 20,
+    (x - window.innerWidth / 2) / 20,
+    1.1
+  ];
+};
+
+const transition3D = (x, y, s) => {
+  return `perspective(1000px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`;
+};
+
+//Component Starts Here
 const CardDisplay = (props) => {
   const {
     mainImg,
@@ -14,38 +29,59 @@ const CardDisplay = (props) => {
     githubCodeLink
   } = props.data;
 
-  return (
-    <div className="workCard">
-      <img src={mainImg} alt="Project Capture" className="sizingImg" />
-      <p className="workCardText">
-        Project {project_id}
-        <br />
-        Name: {name}
-        <br />
-        Description: {description}
-      </p>
-      <div className="projectIcons">
-        <a
-          href={deployLink}
-          target="_blank"
-          rel="noopener noreferrer"
-          title="Visit Project"
-          className="projectLink"
-        >
-          <img src={Icon7} alt="View Laptop Icon" />
-        </a>
+  //Card Slide Animation
+  const [toggle, setToggle] = useState(false);
 
-        <a
-          href={githubCodeLink}
-          target="_blank"
-          rel="noopener noreferrer"
-          title="View Code"
-          className="projectLink"
-        >
-          <img src={Icon8} alt="Code File Icon" />
-        </a>
-      </div>
-    </div>
+  //3D Card Animation
+  const [settings, setSettings] = useSpring(() => ({
+    xys: [0, 0, 1],
+    config: { mass: 1, tension: 150, friction: 50 }
+  }));
+
+  return (
+    <animated.div
+      onClick={() => setToggle(!toggle)}
+      onMouseMove={({ clientX: x, clientY: y }) =>
+        setSettings({ xys: calculate(x, y) })
+      }
+      onMouseLeave={() => setSettings({ xys: [0, 0, 1] })}
+      style={{ transform: settings.xys.interpolate(transition3D) }}
+      className="workCard test"
+    >
+      <img src={mainImg} alt="Project Capture" className="sizingImg" />
+      {toggle ? (
+        <>
+          <p className="workCardText">
+            Project {project_id}
+            <br />
+            Name: {name}
+            <br />
+            Description: {description}
+          </p>
+          <div className="projectIcons">
+            <a
+              href={deployLink}
+              target="_blank"
+              rel="noopener noreferrer"
+              title="Visit Site"
+              className="projectLink"
+            >
+              <img src={Icon7} alt="View Laptop Icon" />
+            </a>
+
+            <a
+              href={githubCodeLink}
+              target="_blank"
+              rel="noopener noreferrer"
+              title="View Code"
+              className="projectLink"
+            >
+              <img src={Icon8} alt="Code File Icon" />
+            </a>
+          </div>
+        </>
+      ) : null}
+    </animated.div>
   );
 };
 
--- portfolio/src/components/Work.js
+++ portfolio/src/components/Work.js
@@ -53,56 +53,6 @@ const Work = () => {
         "This app is under construction at the moment. However, Front-end will be made using React and styling library Styled-Components/React Spring. Back-end is also setup using Node.js.",
       deployLink: "http://www.google.com",
       githubCodeLink: "http://www.google.com"
-    },
-    {
-      id: 6,
-      mainImg: "https://i.postimg.cc/hj1rcwSV/Templatefor-Site.jpg",
-      project_id: "06",
-      name: "Tic Tac Toe App",
-      description:
-        "This app is under construction at the moment. However, Front-end will be made using React and styling library Styled-Components/React Spring. Back-end is also setup using Node.js.",
-      deployLink: "http://www.google.com",
-      githubCodeLink: "http://www.google.com"
-    },
-    {
-      id: 7,
-      mainImg: "https://i.postimg.cc/hj1rcwSV/Templatefor-Site.jpg",
-      project_id: "07",
-      name: "Messaging App",
-      description:
-        "This app is under construction at the moment. However, Front-end will be made using React and styling library Styled-Components/React Spring. Back-end is also setup using Node.js.",
-      deployLink: "http://www.google.com",
-      githubCodeLink: "http://www.google.com"
-    },
-    {
-      id: 8,
-      mainImg: "https://i.postimg.cc/hj1rcwSV/Templatefor-Site.jpg",
-      project_id: "08",
-      name: "Tic Tac Toe App",
-      description:
-        "This app is under construction at the moment. However, Front-end will be made using React and styling library Styled-Components/React Spring. Back-end is also setup using Node.js.",
-      deployLink: "http://www.google.com",
-      githubCodeLink: "http://www.google.com"
-    },
-    {
-      id: 9,
-      mainImg: "https://i.postimg.cc/hj1rcwSV/Templatefor-Site.jpg",
-      project_id: "09",
-      name: "Messaging App",
-      description:
-        "This app is under construction at the moment. However, Front-end will be made using React and styling library Styled-Components/React Spring. Back-end is also setup using Node.js.",
-      deployLink: "http://www.google.com",
-      githubCodeLink: "http://www.google.com"
-    },
-    {
-      id: 10,
-      mainImg: "https://i.postimg.cc/hj1rcwSV/Templatefor-Site.jpg",
-      project_id: "10",
-      name: "Tic Tac Toe App",
-      description:
-        "This app is under construction at the moment. However, Front-end will be made using React and styling library Styled-Components/React Spring. Back-end is also setup using Node.js.",
-      deployLink: "http://www.google.com",
-      githubCodeLink: "http://www.google.com"
     }
   ];
   return (
