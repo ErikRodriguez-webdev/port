d4d9cfff36945c39c81192d2e1384508a62c28e0
Erik Rodriguez <eric19.21.03@gmail.com>
2020-04-08 09:39:34 -0700

    Fixed contact.js copy to clipboard and Removed Image of myself on About.js was placed in digital album instead and Updated color scheme of album

--- portfolio/src/components/About.js
+++ portfolio/src/components/About.js
@@ -1,5 +1,4 @@
 import React from "react";
-import PicOfMe from "../img/myself_img.jpg";
 import ImageDisplay from "./ImageDisplay";
 import "../App.css";
 
@@ -8,9 +7,6 @@ const AboutMe = () => {
     <>
       <h2>About</h2>
       <div className="aboutMainBox">
-        <div className="aboutPhoto">
-          <img src={PicOfMe} alt="Myself" title="Me" className="me" />
-        </div>
         <div className="aboutText">
           <p>
             I'm Erik Rodriguez, a Software Engineer from Moreno Valley,
