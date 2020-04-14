943520d7f7d33c1ed1ca8ae030abbf1f681971dd
Erik Rodriguez <eric19.21.03@gmail.com>
2020-04-13 21:24:32 -0700

    Complete transformation of styles and web app layout

--- portfolio/src/components/Home.js
+++ portfolio/src/components/Home.js
@@ -1,33 +1,15 @@
 import React from "react";
-import backgroundVideo4k from "../vid/backgroundVideo4k.mp4";
-import backgroundStill from "../img/backgroundStill.jpg";
-import { useHistory } from "react-router-dom";
 import "../App.css";
 
 const Home = () => {
-  const history = useHistory();
-
   return (
-    <div className="homeMainBox">
-      <video autoPlay loop muted className="video">
-        <source src={backgroundVideo4k} type="video/mp4" />
-        Your browser does not support the video.
-      </video>
-      <img
-        src={backgroundStill}
-        alt="Background still capture from video"
-        className="mobileBG"
-      />
-      <div className="homeCenterContent">
-        <div className="homeText">
-          <h1>Hello and Welcome to My Port.</h1>
-          <button onClick={() => history.push("/about")}>
-            CLICK HERE TO CONTINUE
-          </button>
-          <p>(WORK IN PROGRESS STILL UNDER CONSTRUCTION)</p>
-        </div>
+    <>
+      <div className="homeMainBox">
+        <h1>Hello and Welcome to My Port.</h1>
+        <button href="#about">CLICK HERE TO CONTINUE</button>
+        <p>(WORK IN PROGRESS STILL UNDER CONSTRUCTION)</p>
       </div>
-    </div>
+    </>
   );
 };
 
