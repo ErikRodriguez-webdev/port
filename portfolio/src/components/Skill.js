b1da60e61051e8e02482c2550b3f87ec7cd66000
Erik Rodriguez <eric19.21.03@gmail.com>
2020-05-08 19:37:52 -0700

    Adds Skill component functionality to tabs and displaying correct text

--- portfolio/src/components/Skill.js
+++ portfolio/src/components/Skill.js
@@ -3,6 +3,22 @@ import SkillIcon from "../img/visible-icon.png";
 import "../App.css";
 
 const Skill = () => {
+  const tabSelector = (tabName) => {
+    const grabActive = document.querySelector(".skillNavBtnActive");
+    grabActive.classList.remove("skillNavBtnActive");
+
+    const grabSelectedTab = document.querySelector(`.${tabName}`);
+    grabSelectedTab.classList.add("skillNavBtnActive");
+
+    const grabTextDisplayed = document.querySelector(".skillDisplayRevealText");
+    grabTextDisplayed.classList.remove("skillDisplayRevealText");
+    grabTextDisplayed.classList.add("skillDisplayHideText");
+
+    const grabNewTextDisplayed = document.querySelectorAll(`.${tabName}`);
+    grabNewTextDisplayed[1].classList.remove("skillDisplayHideText");
+    grabNewTextDisplayed[1].classList.add("skillDisplayRevealText");
+  };
+
   return (
     <section id="skill">
       <div className="skillMainBox">
@@ -14,9 +30,36 @@ const Skill = () => {
         <div className="skillContent">
           <div className="skillNavPlacement">
             <div className="skillNav">
-              <div>Front-End</div>
-              <div>Back-End</div>
-              <div>Tools</div>
+              <div
+                className="skillNavBtn front-end skillNavBtnActive"
+                onClick={() => tabSelector("front-end")}
+              >
+                Front-End
+              </div>
+              <div
+                className="skillNavBtn back-end"
+                onClick={() => tabSelector("back-end")}
+              >
+                Back-End
+              </div>
+              <div
+                className="skillNavBtn tools"
+                onClick={() => tabSelector("tools")}
+              >
+                Tools
+              </div>
+            </div>
+          </div>
+
+          <div className="skillDisplayPlacement">
+            <div className="skillDisplayRevealText front-end">
+              <p>testing front</p>
+            </div>
+            <div className="skillDisplayHideText back-end">
+              <p>testing back</p>
+            </div>
+            <div className="skillDisplayHideText tools">
+              <p>testing tools</p>
             </div>
           </div>
 
