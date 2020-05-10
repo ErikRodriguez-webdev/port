4d3101689bc094ca1da1ddc1eaa0f26380a30470
Erik Rodriguez <eric19.21.03@gmail.com>
2020-05-10 12:58:57 -0700

    Adds WorkCard component to pass down cards into work component and Adds stying to work component and Fixes styling on skill component

--- portfolio/src/components/Story.js
+++ portfolio/src/components/Story.js
@@ -12,7 +12,30 @@ const Story = () => {
         </div>
 
         <div className="storyContent">
-          <p>Content</p>
+          <div className="storyText">
+            <p>
+              It all began when my brother introduced me to shoes and clothing.
+              With this new hobby my brother and I teamed up to start flipping
+              these highly limited and extremely demanded sneakers and clothing
+              pieces. When everyone in the world and their mother is trying to
+              purchase a high ticket item to flip you need any advantage you can
+              get. Advantages can be potential links that can lead you to the
+              item drop, list of all retailers dropping to plan out order of
+              releases to hit, and the best advantage to give yourself is using
+              automated checkout software.
+              <br />
+              In the resell industry you call automated checkout software, bots.
+              Bots can give you a huge advantage to not only secure a single
+              high ticket item, but have hundreds of tasks running trying to
+              checkout and purchase with payment. This led my brother and I to
+              try our luck and learn how to operate one of these bots, so we
+              saved up enough money and bought one. Since there was only one bot
+              my brother decided that he could man the bot and I could manually
+              try going for the items dropping during the learning stages.
+            </p>
+          </div>
+
+          <div className="storyImg">Image COMING SOON</div>
         </div>
       </div>
     </section>
