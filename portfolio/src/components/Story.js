06b9b55f3bde0d21c2b15987e5a05a3b7a911a7b
Erik Rodriguez <eric19.21.03@gmail.com>
2020-05-13 20:57:25 -0700

    Adds media queries for work component at 850px to 300px

--- portfolio/src/components/Story.js
+++ portfolio/src/components/Story.js
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import { storyData } from "../utils/StoryData";
+import LeftIcon from "../img/left-icon.png";
+import RightIcon from "../img/right-icon.png";
 import StoryIcon from "../img/file-icon.png";
-import TwinImage from "../img/broAndI.JPG";
+// import TwinImage from "../img/broAndI.JPG";
 import "../App.css";
 
 const Story = () => {
+  const data = storyData;
+  const [counter, setCounter] = useState(0);
+
+  const forward = () => {
+    if (counter >= 4) {
+      setCounter(0);
+    } else {
+      setCounter(counter + 1);
+    }
+  };
+
+  const back = () => {
+    if (counter <= 0) {
+      setCounter(4);
+    } else {
+      setCounter(counter - 1);
+    }
+  };
+
   return (
     <section id="story">
       <div className="storyMainBox">
@@ -13,32 +35,22 @@ const Story = () => {
         </div>
 
         <div className="storyContent">
-          <div className="storyText">
-            <p>
-              It all began when my brother introduced me to shoes and clothing.
-              With this new hobby my brother and I teamed up to start flipping
-              these highly limited and extremely demanded sneakers and clothing
-              pieces. When everyone in the world and their mother is trying to
-              purchase a high ticket item to flip you need any advantage you can
-              get. Advantages can be potential links that can lead you to the
-              item drop, list of all retailers dropping to plan out order of
-              releases to hit, and the best advantage to give yourself is using
-              automated checkout software.
-              <br />
-              <br />
-              In the resell industry you call automated checkout software, bots.
-              Bots can give you a huge advantage to not only secure a single
-              high ticket item, but have hundreds of tasks running trying to
-              checkout and purchase with payment. This led my brother and I to
-              try our luck and learn how to operate one of these bots, so we
-              saved up enough money and bought one. Since there was only one bot
-              my brother decided that he could man the bot and I could manually
-              try going for the items dropping during the learning stages.
-            </p>
-          </div>
+          <div className="storyCard">
+            <div className="storyNavBtn" onClick={back}>
+              <img src={LeftIcon} alt="left chevron" />
+            </div>
 
-          <div className="storyImg">
-            <img src={TwinImage} alt="My Brother and I " />
+            <div className="storyCenter">
+              <p>{`${counter + 1}/${data.length}`}</p>
+              <div className="storyImage">
+                <img src={data[counter].img} alt="a digital story album" />
+              </div>
+              <p>{data[counter].text}</p>
+            </div>
+
+            <div className="storyNavBtn" onClick={forward}>
+              <img src={RightIcon} alt="left chevron" />
+            </div>
           </div>
         </div>
       </div>
