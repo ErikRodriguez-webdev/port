c04600b52650bd6e9c88e9e225a886cc5cefdce7
Erik Rodriguez <eric19.21.03@gmail.com>
2020-05-14 13:44:19 -0700

    Removes unused dependencies

--- portfolio/src/components/WorkCard.js
+++ portfolio/src/components/WorkCard.js
@@ -4,7 +4,7 @@ import "../App.css";
 const WorkCard = (props) => {
   //Add closerlook and new component to route to
   return (
-    <div className="workCard">
+    <div className="workCard" onClick={() => props.history.push("/work/")}>
       <h2>{props.data.title}</h2>
       <div className="workCardImage">
         <img src={props.data.image} alt="placeholder desktop screen capture" />
