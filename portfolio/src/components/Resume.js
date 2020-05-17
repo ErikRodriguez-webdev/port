52b0ce8d54a6246f41c147f5f2578c6a1519357f
Erik Rodriguez <eric19.21.03@gmail.com>
2020-05-16 18:19:49 -0700

    Adds new resume route to app.js and Adds resume to resume component with styling done

--- portfolio/src/components/Resume.js
+++ portfolio/src/components/Resume.js
@@ -1,11 +1,28 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
+import LeftArrow from "../img/arrow-left.png";
+import MyResume from "../img/Erik_Rodriguez_Resume.pdf";
 import "../App.css";
 
 const Resume = () => {
+  const history = useHistory();
   return (
-    <div>
-      <p>in resume</p>
-    </div>
+    <section className="resumePage">
+      <div className="resumePageCornerTag" onClick={() => history.goBack()}>
+        <div>
+          <img src={LeftArrow} alt="arrow left" />
+        </div>
+        <h3>Go Back</h3>
+      </div>
+      <div className="resumePageTitle">Skill / Resume</div>
+      <iframe
+        src={MyResume}
+        alt="pdf resume"
+        title="Erik Rodriguez Resume"
+        name="Erik Rodriguez Resume"
+        className="resumeDocument"
+      />
+    </section>
   );
 };
 
