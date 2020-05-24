107c5dc397c2b418ede782006ff6f640235aee83
Erik Rodriguez <eric19.21.03@gmail.com>
2020-05-24 14:17:59 -0700

    Adds images and changes text on storyData component

--- portfolio/src/utils/WorkData.js
+++ portfolio/src/utils/WorkData.js
@@ -4,7 +4,7 @@ export const workData = [
     image: "https://i.postimg.cc/1tGrfPLN/capture-Of-Signlingo.jpg",
     title: "SignLingo App",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vel odio justo. Donec eget bibendum orci. Sed sodales, ligula eu bibendum convallis, neque ex sollicitudin mi, ac malesuada purus massa id quam. Suspendisse pulvinar est eget auctor rhoncus. Mauris vel sem eget magna tincidunt imperdiet eu nec purus. Phasellus velit justo, ullamcorper at mollis sit amet, tristique nec lacus. Sed quis libero volutpat libero bibendum placerat eget sed metus. Morbi et justo placerat, elementum orci non, auctor tellus.",
+      "Sed quis libero volutpat libero bibendum placerat eget sed metus. Morbi et justo placerat, elementum orci non, auctor tellus.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vel odio justo. Donec eget bibendum orci. Sed sodales, ligula eu bibendum convallis, neque ex sollicitudin mi, ac malesuada purus massa id quam. Suspendisse pulvinar est eget auctor rhoncus. Mauris vel sem eget magna tincidunt imperdiet eu nec purus. Phasellus velit justo, ullamcorper at mollis sit amet, tristique nec lacus. Sed quis libero volutpat libero bibendum placerat eget sed metus. Morbi et justo placerat, elementum orci non, auctor tellus.",
     deployedLink: "https://master.d2965nx2i7rdu0.amplifyapp.com/",
     projectCode: "https://github.com/Lambda-School-Labs/signlingo-fe",
   },
@@ -13,7 +13,7 @@ export const workData = [
     image: "https://i.postimg.cc/hPHsdZXZ/Templatefor-Site.jpg",
     title: "Something App",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vel odio justo. Donec eget bibendum orci. Sed sodales, ligula eu bibendum convallis, neque ex sollicitudin mi, ac malesuada purus massa id quam. Suspendisse pulvinar est eget auctor rhoncus. Mauris vel sem eget magna tincidunt imperdiet eu nec purus. Phasellus velit justo, ullamcorper at mollis sit amet, tristique nec lacus. Sed quis libero volutpat libero bibendum placerat eget sed metus. Morbi et justo placerat, elementum orci non, auctor tellus.",
+      "Donec eget bibendum orci. Sed sodales, ligula eu bibendum convallis, neque ex sollicitudin mi, ac malesuada purus massa id quam.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vel odio justo. Donec eget bibendum orci. Sed sodales, ligula eu bibendum convallis, neque ex sollicitudin mi, ac malesuada purus massa id quam. Suspendisse pulvinar est eget auctor rhoncus. Mauris vel sem eget magna tincidunt imperdiet eu nec purus. Phasellus velit justo, ullamcorper at mollis sit amet, tristique nec lacus. Sed quis libero volutpat libero bibendum placerat eget sed metus. Morbi et justo placerat, elementum orci non, auctor tellus.",
     deployedLink: "www.google.com/",
     projectCode: "www.google.com/",
   },
