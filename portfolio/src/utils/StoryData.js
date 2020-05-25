e2c79072d771d26cddc28f35d2a9d835180284c9
Erik Rodriguez <eric19.21.03@gmail.com>
2020-05-25 14:11:22 -0700

    Adds image to last story slide and Adds bottom padding to work closerlook

--- portfolio/src/utils/StoryData.js
+++ portfolio/src/utils/StoryData.js
@@ -7,11 +7,11 @@ export const storyData = [
   {
     text:
       "At one point during this time, I ran into an ad on Instagram. I don't know why I decided to stop scrolling on my Instagram feed that day but the caption got my attention. The ad's caption read, 'Attend a school that invests in its students. Click here to learn more about our ISA.' I clicked... and once I was convinced... I talked it over with my brother. That weekend, we signed up.",
-    img: "https://i.postimg.cc/QCHMzqYD/broAndI.jpg",
+    img: "https://i.postimg.cc/rp1gd1Jq/broAndI.jpg",
   },
   {
     text:
       "Fast forward half a year, my brother and I have learned to work with existing websites and how to build them from scratch. We have covered topics like Responsive Design, Single Page Applications, Advanced State Management, etc. Now looking back at how far we have come during these 6 months, I am glad to say that I am happy with my career choice.",
-    img: "https://i.postimg.cc/hPHsdZXZ/Templatefor-Site.jpg",
+    img: "https://i.postimg.cc/s2x5pb5f/work-Files.jpg",
   },
 ];
