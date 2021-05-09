71d3c7d3456230eb8a0e2b1c8f33a37018cf032a
Erik Rodriguez <perhapserik@gmail.com>
2021-05-09 15:17:06 -0700

    • Removes commented out code • Deletes css styles that are no longer needed

--- portfolio/src/components/Resume.js
+++ portfolio/src/components/Resume.js
@@ -1,23 +1,10 @@
 import React from "react";
-import { useHistory } from "react-router-dom";
-import LeftArrow from "../img/arrow-left.png";
 import MyResume from "../img/Erik Rodriguez Resume PDF.pdf";
 import "../App.css";
 
 const Resume = () => {
-  // const history = useHistory();
   return (
-    // <section className="resumePage">
-    //   <div className="resumePageContainer">
-    //     <div className="resumePageCornerTag" onClick={() => history.goBack()}>
-    //       <div>
-    //         <img src={LeftArrow} alt="arrow left" />
-    //       </div>
-    //       <h3>Go Back</h3>
-    //     </div>
-    //     <div className="resumePageTitle">Skill / Resume</div>
-    //   <div className="resumeDocument">
-    <div style={{ width: "100%", height: "100vh" }}>
+    <section style={{ width: "100%", height: "100vh" }}>
       <iframe
         src={MyResume}
         alt="pdf resume"
@@ -30,9 +17,7 @@ const Resume = () => {
         height="100%"
         scrolling="auto"
       />
-    </div>
-    //   </div>
-    // </section>
+    </section>
   );
 };
 
