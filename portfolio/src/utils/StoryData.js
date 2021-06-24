1c5bc1dc9609def5ddb616291700cd85c4560830
Erik Rodriguez <perhapserik@gmail.com>
2021-06-23 21:02:31 -0700

    • Updates story data structure with new story • Adds new story slides

--- portfolio/src/utils/StoryData.js
+++ portfolio/src/utils/StoryData.js
@@ -1,17 +1,22 @@
 export const storyData = [
   {
     id: 1,
-    text: "My professional career started when I dropped out of college in 2019. For the longest, I was unsure of what I wanted to do for the rest of my life. For that reason, job searching became my temporary job to find the right job for me.",
+    text: "My professional career started after I quit multiple dead-end jobs. I wasn't sure what I wanted, so I kept searching for a job I could be passionate about, similar to the military. During this time, I was a Reseller where I flip products on sites like eBay. (Photo: Moyo)",
     img: "https://i.postimg.cc/ncFNfyMk/myCat.jpg",
   },
   {
     id: 2,
-    text: "At some point during this time, I ran into an ad on Instagram. I'm not sure why I decided to stop scrolling on my Instagram feed that day, but the ad's caption got my attention. The caption read, 'Attend a school that invests in its students. Click here to learn more about our ISA.' I clicked... and once I was convinced... I talked it over with my brother. That weekend, we signed up.",
+    text: "Some Resellers use Bots, these are programs that help you buy products insanely fast. My interest in programming grew from this point on because of Bots. A week later, I ran into an ad on Instagram. I'm not sure why I decided to stop scrolling on my feed that day, but the ad's caption got my attention. (Photo: Erik on Left and my brother on Right)",
     img: "https://i.postimg.cc/rp1gd1Jq/broAndI.jpg",
   },
   {
     id: 3,
-    text: "Fast forward half a year, my brother and I have learned how to build websites from scratch, continue development on existing sites, and collaborate with cross-functional teams. We have covered topics like Responsive Design, Single Page Applications, Advanced State Management, etc. Reflecting on how far we have come during these six months, I am glad to say that I am happy with my career choice.",
+    text: "So I clicked... and once I was convinced... I talked it over with my brother. That weekend, we signed up for Lambda. Fast forward half a year, my brother and I have learned how to build/continue development on existing web applications. We have covered topics like Responsive Design, Single Page Applications, Advanced State Management, Computer Science, etc. (Photo: Working on Portfolio)",
     img: "https://i.postimg.cc/s2x5pb5f/work-Files.jpg",
+  },
+  {
+    id: 4,
+    text: "Reflecting on how much we have grown in the last year, I am glad to say that I am thrilled with my career choice. My next objective is to break into the industry as a Full Stack Web Developer. (Photo: Screen Capture of Resume)",
+    img: "https://i.postimg.cc/QtTftHR2/story-Resume.png",
   },
 ];
