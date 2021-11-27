8c333d15f2f2e5dc8bb306c83494aa3329e31263
Erik Rodriguez <perhapserik@gmail.com>
2021-11-26 17:59:17 -0800

    • Set story data structure for story cards

---
+++ portfolio/src/components/Story/storyData.js
@@ -0,0 +1,21 @@
+import catImg from "../../img/myCat.JPG";
+import workImg from "../../img/workFiles.JPG";
+
+export const storyData = [
+  {
+    id: 1,
+    text1:
+      "My professional career started after I dropped out of college. I wasn't sure what I wanted, so I kept searching for a job I could be passionate about, similar to the military. (Photo: Moyo)",
+    text2:
+      "I ran into an ad on Instagram one day. I'm not sure why I decided to stop scrolling on my feed that day, but the ad's caption got my attention. So I clicked... and once I was convinced... I talked it over with my brother. That weekend, we signed up for Lambda. ",
+    image: catImg,
+  },
+  {
+    id: 2,
+    text1:
+      "Fast forward half a year, my brother and I have learned how to build/continue development on existing web applications. We have covered topics like Responsive Design, Single Page Applications, Advanced State Management, Computer Science, etc. (Photo: Working on Portfolio v1)",
+    text2:
+      "Reflecting on how much we have grown in the last year, I am glad to say that I am thrilled with my career choice. My next objective is to break into the industry as a Full Stack Web Developer.",
+    image: workImg,
+  },
+];
