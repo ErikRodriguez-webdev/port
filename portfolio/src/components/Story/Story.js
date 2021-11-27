75403f6b40234a538c8417501f88f795504ca1d4
Erik Rodriguez <perhapserik@gmail.com>
2021-11-26 18:18:26 -0800

    • Builds story component to match wireframe look

--- portfolio/src/components/Story/Story.js
+++ portfolio/src/components/Story/Story.js
@@ -1,3 +1,5 @@
+import { storyData } from "./storyData";
+
 import { createStyles, Typography } from "@mui/material";
 import { makeStyles } from "@mui/styles";
 import { Box } from "@mui/system";
@@ -5,8 +7,21 @@ import { Box } from "@mui/system";
 const useStyles = makeStyles((theme) =>
   createStyles({
     storyContainer: {
-      border: "1px solid blue",
       padding: "5% 0",
+    },
+    storyTitle: {
+      fontWeight: 700,
+      marginBottom: "5px",
+    },
+    storyCard: {
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      margin: "2% 0",
+    },
+    storyText: {
+      width: "35%",
+      marginRight: "5%",
     },
   })
 );
@@ -15,9 +30,23 @@ const Story = (props) => {
   const classes = useStyles(props.theme);
 
   return (
-    <div className={classes.storyContainer}>
-      <p>testing</p>
-    </div>
+    <section className={classes.storyContainer}>
+      <Typography variant="h4" component="h2" className={classes.storyTitle}>
+        My Story
+      </Typography>
+
+      {storyData.map(({ id, text1, text2, image }) => (
+        <Box key={id} className={classes.storyCard}>
+          <Box className={classes.storyText}>
+            <Typography>{text1}</Typography>
+            <br />
+            <br />
+            <Typography>{text2}</Typography>
+          </Box>
+          <img src={image} alt="image of my story" />
+        </Box>
+      ))}
+    </section>
   );
 };
 
