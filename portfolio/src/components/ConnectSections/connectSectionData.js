0ab143a0ef5c151cd588f1c5c1a1e61668026e9c
Erik Rodriguez <perhapserik@gmail.com>
2021-11-28 16:50:36 -0800

    • Makes a data structure for sections to connect

---
+++ portfolio/src/components/ConnectSections/connectSectionData.js
@@ -0,0 +1,18 @@
+import About from "../About/About.js";
+import Work from "../Work/Work.js";
+import Story from "../Story/Story.js";
+
+export const connectSectionData = [
+  {
+    id: 1,
+    section: <About />,
+  },
+  {
+    id: 2,
+    section: <Work />,
+  },
+  {
+    id: 3,
+    section: <Story />,
+  },
+];
