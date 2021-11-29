7bf1ef83e89c6b2e3886bb8ca01fefe18d23e1aa
Erik Rodriguez <perhapserik@gmail.com>
2021-11-28 16:54:43 -0800

    • Builds connect sections component matching figma wireframe

--- portfolio/src/components/ConnectSections/ConnectSections.js
+++ portfolio/src/components/ConnectSections/ConnectSections.js
@@ -1,8 +1,29 @@
+import { connectSectionData } from "./connectSectionData";
+
+import {
+  Timeline,
+  TimelineConnector,
+  TimelineContent,
+  TimelineDot,
+  TimelineItem,
+  TimelineSeparator,
+} from "@mui/lab";
+
 const ConnectSections = (props) => {
+  const dataLength = connectSectionData.length;
+
   return (
-    <>
-      <p>in connect file</p>
-    </>
+    <Timeline>
+      {connectSectionData.map(({ id, section }) => (
+        <TimelineItem key={id}>
+          <TimelineSeparator>
+            <TimelineDot />
+            {id !== dataLength && <TimelineConnector />}
+          </TimelineSeparator>
+          <TimelineContent>{section}</TimelineContent>
+        </TimelineItem>
+      ))}
+    </Timeline>
   );
 };
 
