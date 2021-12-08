37d1a58429ef53d33c91cb3809fc6e22988ade45
Erik Rodriguez <perhapserik@gmail.com>
2021-12-08 14:31:31 -0800

    • Edits storyData photo credit

--- portfolio/src/components/Story/storyData.js
+++ portfolio/src/components/Story/storyData.js
@@ -5,17 +5,17 @@ export const storyData = [
   {
     id: 1,
     text1:
-      "My professional career started after I dropped out of college. I wasn't sure what I wanted, so I kept searching for a job I could be passionate about, similar to the military. (Photo: Moyo)",
+      "My professional career started after I dropped out of college. I wasn't sure what I wanted, so I kept searching for a job I could be passionate about, similar to the military.",
     text2:
-      "I ran into an ad on Instagram one day. I'm not sure why I decided to stop scrolling on my feed that day, but the ad's caption got my attention. So I clicked... and once I was convinced... I talked it over with my brother. That weekend, we signed up for Lambda. ",
+      "I ran into an ad on Instagram one day. I'm not sure why I decided to stop scrolling on my feed that day, but the ad's caption got my attention. So I clicked... and once I was convinced... I talked it over with my brother. That weekend, we signed up for Lambda. (Photo: Moyo)",
     image: catImg,
   },
   {
     id: 2,
     text1:
-      "Fast forward half a year, my brother and I have learned how to build/continue development on existing web applications. We have covered topics like Responsive Design, Single Page Applications, Advanced State Management, Computer Science, etc. (Photo: Working on Portfolio v1)",
+      "Fast forward half a year, my brother and I have learned how to build/continue development on existing web applications. We have covered topics like Responsive Design, Single Page Applications, Advanced State Management, Computer Science, etc.",
     text2:
-      "Reflecting on how much we have grown in the last year, I am glad to say that I am thrilled with my career choice. My next objective is to break into the industry as a Full Stack Web Developer.",
+      "Reflecting on how much we have grown in the last year, I am glad to say that I am thrilled with my career choice. My next objective is to break into the industry as a Full Stack Web Developer. (Photo: Working on Portfolio v1)",
     image: workImg,
   },
 ];
