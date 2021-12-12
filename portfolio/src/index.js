9b035f957ec697af7895110453a436d5c6b54d06
Erik Rodriguez <perhapserik@gmail.com>
2021-12-11 16:18:16 -0800

    • Creates material ui theme

--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -1,15 +1,77 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
 //Router
 import { BrowserRouter as Router } from "react-router-dom";
 
+//MUI
+import { createTheme, ThemeProvider } from "@mui/material";
+import { grey } from "@mui/material/colors";
+
+const theme = createTheme({
+  palette: {
+    mode: "light",
+    primary: {
+      main: grey[900],
+    },
+    secondary: {
+      main: grey[500],
+    },
+  },
+  breakpoints: {
+    values: {
+      mobile: 550,
+      tablet: 750,
+      laptop: 1000,
+    },
+  },
+  components: {
+    MuiAccordionSummary: {
+      styleOverrides: {
+        expandIconWrapper: {
+          transform: "rotate(45deg)",
+        },
+      },
+    },
+    MuiTimelineItem: {
+      styleOverrides: {
+        root: {
+          ":before": { content: "none" },
+        },
+      },
+    },
+    MuiTimelineContent: {
+      styleOverrides: {
+        root: {
+          padding: "5px",
+        },
+      },
+    },
+    MuiTimeline: {
+      styleOverrides: {
+        root: {
+          padding: "5px",
+        },
+      },
+    },
+  },
+  typography: {
+    body1: {
+      fontSize: "1.3rem",
+    },
+    button: {
+      fontSize: "1.1rem",
+    },
+  },
+});
+
 ReactDOM.render(
   <Router>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </Router>,
   document.getElementById("root")
 );
