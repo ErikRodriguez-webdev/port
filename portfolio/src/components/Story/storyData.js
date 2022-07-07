76f5525592b59448279a500e386c7010c374cfe1
Erik Rodriguez <perhapserik@gmail.com>
2022-07-07 16:37:37 -0700

    • Adds new story content and tweaks existing story content

--- portfolio/src/components/Story/storyData.js
+++ portfolio/src/components/Story/storyData.js
@@ -1,5 +1,6 @@
 import catImg from "../../img/myCat.JPG";
 import workImg from "../../img/workFiles.JPG";
+import azureTeam from "../../img/azureTeam.jpeg";
 
 export const storyData = [
   {
@@ -17,5 +18,13 @@ export const storyData = [
     text2:
       "Reflecting on how much we have grown in the last year, I am glad to say that I am thrilled with my career choice. My next objective is to break into the industry as a Software Engineer. (Photo: Working on Portfolio v1)",
     image: workImg,
+  },
+  {
+    id: 3,
+    text1:
+      "I got the job! Let's goooooo! It took me a little over 1 year, 600+ applications, to get 8 interviews, made it to 2 final interviews, got rejected at Dropbox and got accepted at Microsoft.",
+    text2:
+      "I really enjoy the team and project I was placed into, so I'm hoping to be there long-term. Service Fabric Rocks!!! (Photo: Cloud Service Icon for Service Fabric)",
+    image: azureTeam,
   },
 ];
